e23803d97e7a968b1de4add4f394fbf99c4ded9c
Jack Zhang <zhangmengyu10@gmail.com>
2018-01-31 12:43:54 -0800

    Improved export function basic structure Finished share note function Added todo for future tasks

--- app/src/main/java/com/jackz314/todo/MainActivity.java
+++ app/src/main/java/com/jackz314/todo/MainActivity.java
@@ -37,9 +37,14 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.os.Handler;
+import android.os.ParcelFileDescriptor;
 import android.os.Vibrator;
+import android.print.PageRange;
 import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
 import android.print.PrintManager;
 import android.print.pdf.PrintedPdfDocument;
 import android.speech.RecognizerIntent;
@@ -109,6 +114,7 @@ import com.jackz314.todo.util.IabResult;
 import com.jackz314.todo.util.Inventory;
 import com.jackz314.todo.util.Purchase;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
@@ -2082,6 +2088,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     public boolean exportOrPrint(){//print list or export as pdf
         //todo print list or export as pdf
+        Toast.makeText(getApplicationContext(),getString(R.string.exporting),Toast.LENGTH_SHORT).show();
         String exportBody = null;
         StringBuilder exportBodyBuilder = new StringBuilder();
         exportBody = getString(R.string.note_export_content_header);
@@ -2089,13 +2096,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             exportBodyBuilder.append(data);
             exportBodyBuilder.append("\n\n");//empty line after each note
         }
-        exportBody = exportBodyBuilder.toString();
-        PdfDocument document = new PdfDocument();
+        exportBody = exportBodyBuilder.toString();//this is the final string for export
+        PrintManager printManager = (PrintManager) MainActivity.this
+                .getSystemService(Context.PRINT_SERVICE);
+        String jobName = MainActivity.this.getString(R.string.app_name) + " Document";
+        try{
+            printManager.print(jobName, new ExportPrintAdapter(MainActivity.this),null);//print with print adapter
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return false;
+        }
         //todo change this part!
         //PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(new Rect(0, 0, 100, 100), 1).create();
         //PdfDocument.Page page = document.startPage(pageInfo);
         //document.writeTo();
-        return false;
+        return true;
     }
 
     public void shareSetOfData(){//share note function
@@ -2103,16 +2119,165 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         sharingIntent.setType("text/plain");
         String shareBody = null;
         StringBuilder shareBodyBuilder = new StringBuilder();
-        shareBody = getString(R.string.note_share_content_header);
-        for(String data : selectedContent){
-            shareBodyBuilder.append(data);
-            shareBodyBuilder.append("\n\n");//empty line after each note
+        if (selectedContent.size() == 1){
+            shareBodyBuilder.append(selectedContent.get(0));
+        }else {
+            for(String data : selectedContent){
+                shareBodyBuilder.append("\n\n");//empty line after each note
+                shareBodyBuilder.append(data);
+            }
+            shareBodyBuilder.deleteCharAt(0);//remove the extra final empty lines.
+            shareBodyBuilder.deleteCharAt(0);//remove the extra final empty lines.
         }
         shareBody = shareBodyBuilder.toString();
         String shareSub = getString(R.string.note_share_subject);
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
         startActivity(Intent.createChooser(sharingIntent, getString(R.string.share_via)));
+    }
+
+    public class ExportPrintAdapter extends PrintDocumentAdapter//print adapter for exportation
+    {
+        Context context;
+        private int pageHeight;
+        private int pageWidth;
+        public PdfDocument myPdfDocument;
+        public int totalpages = 1;
+
+        public ExportPrintAdapter(Context context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        public void onLayout(PrintAttributes oldAttributes,
+                             PrintAttributes newAttributes,
+                             CancellationSignal cancellationSignal,
+                             LayoutResultCallback callback,
+                             Bundle metadata) {
+            myPdfDocument = new PrintedPdfDocument(context, newAttributes);//create new PDF document
+
+            pageHeight = newAttributes.getMediaSize().getHeightMils()/1000 * 72;
+            pageWidth = newAttributes.getMediaSize().getWidthMils()/1000 * 72;//calculate page height/width
+
+            if (cancellationSignal.isCanceled() ) {//handle cancellation requests
+                callback.onLayoutCancelled();
+                return;
+            }
+            totalpages = computePageCount(newAttributes);//get total page number
+            if (totalpages > 0) {
+                PrintDocumentInfo.Builder builder = new PrintDocumentInfo
+                        .Builder("todo_export.pdf")//exported file name
+                        .setContentType(PrintDocumentInfo.CONTENT_TYPE_DOCUMENT)
+                        .setPageCount(totalpages);//set page number
+
+                PrintDocumentInfo info = builder.build();
+                callback.onLayoutFinished(info, true);
+            } else {//handle page number == 0
+                callback.onLayoutFailed("Page count is zero.");
+            }
+        }
+
+
+        @Override
+        public void onWrite(final PageRange[] pageRanges,
+                            final ParcelFileDescriptor destination,
+                            final CancellationSignal cancellationSignal,
+                            final PrintDocumentAdapter.WriteResultCallback callback) {//render final export document
+            for (int i = 0; i < totalpages; i++) {
+                if (pageInRange(pageRanges, i))
+                {
+                    PdfDocument.PageInfo newPage = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, i).create();
+
+                    PdfDocument.Page page = myPdfDocument.startPage(newPage);
+
+                    if (cancellationSignal.isCanceled()) {
+                        callback.onWriteCancelled();
+                        myPdfDocument.close();
+                        myPdfDocument = null;
+                        return;
+                    }
+                    drawPage(page, i);
+                    myPdfDocument.finishPage(page);
+                }
+            }
+
+            try {
+                myPdfDocument.writeTo(new FileOutputStream(
+                        destination.getFileDescriptor()));
+            } catch (IOException e) {
+                callback.onWriteFailed(e.toString());
+                return;
+            } finally {
+                myPdfDocument.close();
+                myPdfDocument = null;
+            }
+            callback.onWriteFinished(pageRanges);
+            setOutOfSelectionMode();
+        }
+
+        private void drawPage(PdfDocument.Page page,
+                              int pagenumber) {//todo change this draw page method
+            Canvas canvas = page.getCanvas();
+
+            pagenumber++; // Make sure page numbers start at 1
+
+            int titleBaseLine = 72;
+            int leftMargin = 54;
+
+            Paint paint = new Paint();
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(40);
+            canvas.drawText(
+                    "Test Print Document Page " + pagenumber,
+                    leftMargin,
+                    titleBaseLine,
+                    paint);
+
+            paint.setTextSize(14);
+            canvas.drawText("This is some test content to verify that custom document printing works", leftMargin, titleBaseLine + 35, paint);
+
+            if (pagenumber % 2 == 0)
+                paint.setColor(Color.RED);
+            else
+                paint.setColor(Color.GREEN);
+
+            PdfDocument.PageInfo pageInfo = page.getInfo();
+
+
+            canvas.drawCircle(pageInfo.getPageWidth()/2,
+                    pageInfo.getPageHeight()/2,
+                    150,
+                    paint);
+        }
+
+        private boolean pageInRange(PageRange[] pageRanges, int page)
+        {
+            for (int i = 0; i<pageRanges.length; i++)
+            {
+                if ((page >= pageRanges[i].getStart()) &&
+                        (page <= pageRanges[i].getEnd()))
+                    return true;
+            }
+            return false;
+        }
+
+        private int computePageCount(PrintAttributes printAttributes) { //calculate total page number todo improve this algorithm
+            int itemsPerPage = 4; // default item count for portrait mode
+
+            PrintAttributes.MediaSize pageSize = printAttributes.getMediaSize();
+            if (!pageSize.isPortrait()) {
+                // Six items per page in landscape orientation
+                itemsPerPage = 6;
+            }
+
+            // Determine number of print items
+            int printItemCount = selectedContent.size();
+            return 2; //todo temporary, change later
+            //return (int) Math.ceil(printItemCount / itemsPerPage);
+        }
+
+
     }
 
     public void hideKeyboard() {
